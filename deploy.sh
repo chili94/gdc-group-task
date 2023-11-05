d7883aa2f3a01a52bc826b98631d76c2618f7613
root <root@6e62c5e09d1c.mylabserver.com>
2023-11-05 19:26:27 +0000

    fixing delpoy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-IMAGE_NAME="app-php"
-# Define the initial version
-BUILD_VERSION="1.0.1"
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root."
+  exit 1
+fi
 
-YOUR_DOCKERHUB_NAME="goran94hub"
+# Read the IMAGE_NAME from the command line
+read -p "Enter the IMAGE_NAME which you would like to deploy (e.g., app-php): " IMAGE_NAME
 
-docker pull "$YOUR_DOCKERHUB_NAME/$IMAGE_NAME:$BUILD_VERSION"
+# Read the BUILD_VERSION from the command line
+read -p "Enter the BUILD_VERSION (e.g., 1.0.1 or latest): " BUILD_VERSION
+
+# Read the YOUR_DOCKERHUB_NAME from the command line
+read -p "Enter your Docker Hub username: " YOUR_DOCKERHUB_NAME
+
+if docker image inspect "$YOUR_DOCKERHUB_NAME/$IMAGE_NAME:$BUILD_VERSION" &> /dev/null; then
+  echo "Docker image "$YOUR_DOCKERHUB_NAME/$IMAGE_NAME:$BUILD_VERSION" already exists locally."
+else
+  # If the image is not found locally, pull it from Docker Hub
+   docker pull "$YOUR_DOCKERHUB_NAME/$IMAGE_NAME:$BUILD_VERSION"
+fi
+
